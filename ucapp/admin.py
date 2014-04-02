21f51849fdbc76c4e0b048488f6f1cbcdeadf717
Eric Xiao <exiao@buynear.me>
2014-04-02 13:42:22 -0700

    admin

--- ucapp/admin.py
+++ ucapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from models import Analyst, AnalystFirm, AnalystReview, AnalystRating
 
 # Register your models here.
+
+admin.site.register(AnalystReview)
+admin.site.register(Analyst)
+admin.site.register(AnalystFirm)
+admin.site.register(AnalystRating)
