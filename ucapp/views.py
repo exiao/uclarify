219ed5394ad083bd35772ca09aad3c55ac215a48
Tony Hung <hungyukao92@gmail.com>
2014-04-05 22:47:56 -0700

    Analyst page

--- ucapp/views.py
+++ ucapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
+from models import Analyst
 
 # Create your views here.
 def home(request):
     return render_to_response("home.html", {}, context_instance=RequestContext(request))
 
 def analyst(request):
-    return render_to_response("analyst.html", {}, context_instance=RequestContext(request))
+	analysts = Analyst.objects.all()
+	return render_to_response("analyst.html", {'analysts': analysts}, context_instance=RequestContext(request))
