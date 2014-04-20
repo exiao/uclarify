ab4bebe2010cffe981f43a660cd77b3568fb6ae8
seung lee <seung.j@Live.com>
2014-04-19 23:45:48 -0700

    re ordered raty

--- static/js/review_analyst.js
+++ static/js/review_analyst.js
@@ -2,25 +2,25 @@ $(document).ready(function () {
 
 
     $("#rate-1").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-2").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-3").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-4").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-5").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-6").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
     $("#rate-7").raty({
-        path: STATIC_URL + "plugins/raty-2.5.2/lib/img"
+        path: STATIC_URL + "plugins/raty-2.5.2/img"
     });
 
 
