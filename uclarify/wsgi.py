277560db5d75c980687ad2712f1a73e348ff974f
Eric Xiao <exiao@buynear.me>
2014-05-08 05:09:06 -0700

    heroku

--- uclarify/wsgi.py
+++ uclarify/wsgi.py
@@ -12,7 +12,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "uclarify.settings")
 
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
+application = get_wsgi_application()
 
 # THIS IS HEROKU
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
