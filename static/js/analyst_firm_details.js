87dd04b44cb2fad4194d789b46c94f4e11aca638
Tony Hung <hungyukao92@gmail.com>
2014-05-15 17:39:11 -0700

    Add tooltip js file

---
+++ static/js/analyst_firm_details.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    $(".rating-wrapper .description").tooltip({
+	    placement: "right",
+	    trigger: "hover"
+	});
+
+	$(".strength").tooltip({
+	    placement: "right",
+	    trigger: "hover"
+	});
+
+	$(".basic-info .rating").tooltip({
+	    placement: "right",
+	    trigger: "hover"
+	});
+});
